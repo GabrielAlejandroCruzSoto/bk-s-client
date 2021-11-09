3af639d9449ca20d080fd24bab10efe1e94a9ebb
Gabriel Alejandro Cruz Soto <gabrielcrsoto@outlook.com>
2021-11-09 13:44:36 -0300

    proyect Initial

---
+++ src/main/java/cl/gabrielcrsoto/bksclient/BkSClientApplication.java
@@ -0,0 +1,13 @@
+package cl.gabrielcrsoto.bksclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkSClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkSClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/gabrielcrsoto/bksclient/BkSClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.gabrielcrsoto.bksclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkSClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
